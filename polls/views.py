28bbc4337e7040bb045737dfb6a3d3a23eaab869
Gerard Causse <=>
2025-02-10 19:28:43 -0500

    Improving our view

--- polls/views.py
+++ polls/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import get_object_or_404, render
 
 # from django.template import loader
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 
 from polls.models import Choice, Question
@@ -19,7 +20,14 @@ class IndexView(generic.ListView):
 
     def get_queryset(self):
         """Return the last five published questions."""
-        return Question.objects.order_by("-pub_date")[:5]
+        """
+    Return the last five published questions (not including those set to be
+    published in the future).
+    """
+        # return Question.objects.order_by("-pub_date")[:5]
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by(
+            "-pub_date"
+        )[:5]
 
 
 # def index(request):
@@ -84,4 +92,6 @@ def vote(request, question_id):
         # Always return an HttpResponseRedirect after successfully dealing
         # with POST data. This prevents data from being posted twice if a
         # user hits the Back button.
-        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+        return HttpResponseRedirect(
+            reverse("polls:results", args=(question.id,))
+        )
